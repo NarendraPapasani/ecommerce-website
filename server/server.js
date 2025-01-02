8adb6bf6ed7c7e2c4729379f27e603e3ffd325bf
Narendra <papasaninarendra9492@gmail.com>
2025-01-02 23:22:00 +0530

    change3

--- server/server.js
+++ server/server.js
@@ -29,9 +29,6 @@ app.use("/api/products", require("./routes/productRoute"));
 app.use("/api/cart", authenticateController, require("./routes/cartRoute"));
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
 
 const PORT = process.env.PORT || 8000;
 
