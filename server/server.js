bf8e908744f7142aa6c1120c09f98ed689c758a0
Narendra <papasaninarendra9492@gmail.com>
2025-01-03 16:02:15 +0530

    Update catchall route to serve React app from the dist directory

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use("/api/order", authenticateController, require("./routes/orderRoute"));
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist"));
 });
 
 const PORT = process.env.PORT || 8000;
