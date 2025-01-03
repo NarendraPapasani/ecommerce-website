f3f8541dcc16ce32b2d38d4a22a81ac8bf7f90f8
Narendra <papasaninarendra9492@gmail.com>
2025-01-03 13:14:30 +0530

    Update CORS origin to allow requests from local development server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const connectDb = require("./DB/connectDb");
 
 app.use(
   cors({
-    origin: "https://shiny-dasik-9cf343.netlify.app/",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
