2b6b1ee34d476be6fb5ffb4ae7365e60711d6163
Narendra <papasaninarendra9492@gmail.com>
2025-01-03 17:29:40 +0530

    Add logging for successful product fetch in productController

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,6 +3,7 @@ const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json({ products });
+    console.log("Products fetched successfully");
   } catch (error) {
     console.error("Error fetching products:", error);
     res.status(500).json({ msg: "Server error" });
