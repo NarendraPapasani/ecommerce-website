e3b09ed234de973ecc77ae98df150ba8374e604e
Narendra <papasaninarendra9492@gmail.com>
2025-01-06 10:50:12 +0530

    Serve static files and handle client-side routing in production environment

--- server/server.js
+++ server/server.js
@@ -31,6 +31,14 @@ app.use(
 );
 app.use("/api/order", authenticateController, require("./routes/orderRoute"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 connectDb()
